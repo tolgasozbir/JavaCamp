fe33836ead60ea09831321b60b1189664b56806f
tolgasozbir <tolgasoz1@gmail.com>
2021-11-22 19:47:54 +0300

    Interfaces

---
+++ src/Interfaces/Main.java
@@ -0,0 +1,16 @@
+package Interfaces;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Logger[] loggers={new EmailLogger(),new SmsLogger(),new FileLogger()};
+
+        CustomerManager customerManager = new CustomerManager(loggers);
+
+        Customer customer1 =new Customer(1,"Tolga","Sözbir");
+
+        customerManager.add(customer1);
+        System.out.println("//");
+        customerManager.delete(customer1);
+    }
+}
